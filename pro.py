aa7b392030622051203f72ab6e9160eb953920ce
Noob Stranger <85062690+Noob-Stranger@users.noreply.github.com>
2021-08-22 18:06:02 +0530

    --host=0.0.0.0 --port=5000

--- pro.py
+++ pro.py
@@ -1,3 +1,3 @@
 import os
-os.system("git clone https://github.com/teamspeedo/speedo.git && cd speedo && python3 -m main_start")
+os.system("git clone https://github.com/teamspeedo/speedo.git && cd speedo && python3 -m main_start --host=0.0.0.0 --port=5000")
 # Dockerised | Heroku Support bt Noob Stranger
